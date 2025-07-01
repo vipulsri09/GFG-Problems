a3bbf10bf41127ffd994b436d4c1882c5c7b6f10
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-07-01 16:00:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.java
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -1,16 +1,19 @@
 class Solution {
     public int startStation(int[] gas, int[] cost) {
-        int n=gas.length;
-        int totalGas=0, totalCost=0, tank=0, start=0;
-        for(int i=0; i<n; i++){
-            totalGas+=gas[i];
-            totalCost+=cost[i];
-            tank=tank + gas[i] - cost[i];
-            if(tank<0){
-                start=i+1;
-                tank=0;
+        int totalGas = 0, totalCost = 0, tank = 0, start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
             }
         }
-        return totalGas >= totalCost ? start : -1;
+        if (totalGas >= totalCost) {
+            return start;
+        } else {
+            return -1;
+        }
     }
 }
